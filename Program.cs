7b26bd4d9bfa5114a15e2262da7a486bef6a2f93
morganssch <coltm576@gmail.com>
2026-02-23 09:56:54 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Casting_Morgan_Joseph
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("How many pets do you have?");
+            string numberOfUserPets = Console.ReadLine();
+            Console.WriteLine($"You have {numberOfUserPets} pets! That's great!");
+
+            Console.WriteLine("You like dogs? (true/false)");
+            bool userLikesDogs = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine($"You like dogs? {userLikesDogs}");
+
+
+        }
+    }
+}
